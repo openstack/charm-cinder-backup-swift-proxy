3a63c1a2f81837398f5fc27a3b95c805c813d2f4
Yoshi Kadokawa <yoshi.kadokawa@canonical.com>
2018-11-27 21:15:56 +0900

    change backend from ceph to external swift service

--- hooks/cinder_backup_contexts.py
+++ hooks/cinder_backup_contexts.py
@@ -12,11 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
-
 from charmhelpers.core.hookenv import (
-    service_name,
-    is_relation_made,
+    config,
 )
 from charmhelpers.contrib.openstack.context import (
     OSContextGenerator,
@@ -27,35 +24,63 @@ from charmhelpers.contrib.openstack.utils import (
 )
 
 
-class CephBackupSubordinateContext(OSContextGenerator):
-    interfaces = ['ceph-cinder']
-
+class SwiftBackupSubordinateContext(OSContextGenerator):
     def __call__(self):
-        """Used to generate template context to be added to cinder.conf in the
-        presence of a ceph relation.
+        """Used to generate template context to be added to cinder.conf.
         """
-        if not is_relation_made('ceph', 'key'):
-            return {}
 
         release = get_os_codename_package('cinder-common')
-        if CompareOpenStackReleases(release) < "icehouse":
+        if CompareOpenStackReleases(release) < "queens":
             raise Exception("Unsupported version of Openstack")
 
-        service = service_name()
-        backup_driver = 'cinder.backup.drivers.ceph'
-        return {
-            "cinder": {
-                "/etc/cinder/cinder.conf": {
-                    "sections": {
-                        'DEFAULT': [
-                            ('backup_driver', backup_driver),
-                            ('backup_ceph_conf',
-                                os.path.join('/var/lib/charm',
-                                             service, 'ceph.conf')),
-                            ('backup_ceph_pool', service),
-                            ('backup_ceph_user', service),
-                        ]
+        backup_driver = 'cinder.backup.drivers.swift.SwiftBackupDriver'
+        backup_auth_method = 'single_user'
+        if config('auth-version') == 2:
+            ctxt = {
+                "cinder": {
+                    "/etc/cinder/cinder.conf": {
+                        "sections": {
+                            'DEFAULT': [
+                                ('backup_driver', backup_driver),
+                                ('backup_swift_auth', backup_auth_method),
+                                ('backup_swift_auth_version', config('auth-version')),
+                                ('backup_swift_url', config('endpoint-url')),
+                                ('backup_swift_auth_url', config('auth-url')),
+                                ('backup_swift_user', config('swift-user')),
+                                ('backup_swift_key', config('swift-key')),
+                                ('backup_swift_container', config('container-name')),
+                                ('backup_swift_object_size', config('object-size')),
+                                ('backup_swift_block_size', config('block-size')),
+                                ('backup_swift_tenant', config('tenant-name')),
+                            ]
+                        }
                     }
                 }
             }
-        }
+        elif config('auth-version') == 3:
+            ctxt = {
+                "cinder": {
+                    "/etc/cinder/cinder.conf": {
+                        "sections": {
+                            'DEFAULT': [
+                                ('backup_driver', backup_driver),
+                                ('backup_swift_auth', backup_auth_method),
+                                ('backup_swift_auth_version', config('auth-version')),
+                                ('backup_swift_url', config('endpoint-url')),
+                                ('backup_swift_auth_url', config('auth-url')),
+                                ('backup_swift_user', config('swift-user')),
+                                ('backup_swift_key', config('swift-key')),
+                                ('backup_swift_container', config('container-name')),
+                                ('backup_swift_object_size', config('object-size')),
+                                ('backup_swift_block_size', config('block-size')),
+                                ('backup_swift_user_domain', config('user-domain')),
+                                ('backup_swift_project_domain', config('project-domain')),
+                                ('backup_swift_project', config('project-name')),
+                            ]
+                        }
+                    }
+                }
+            }
+        else:
+            raise Exception("Unsupported swift auth version")
+        return ctxt
